ce99d68001f22a1b80f274a056984e37c6b05381
zolda93 <aybayb9393@gmail.com>
2024-09-18 14:24:00 +0100

    add generation functions

--- elysium/__init__.py
+++ elysium/__init__.py
@@ -49,5 +49,31 @@ class no_grad:
 
 from .tensor import *
 
+def tensor(data,requires_grad=False,**kwargs):
+    if isinstance(data,Tensor):return Tensor(data.data,requires_grad=requires_grad,**kwargs)
+    return Tensor(data,requires_grad=requires_grad,**kwargs)
+def zeros(*shape,requires_grad=False,**kwargs):
+    xp = cp  if (cp is not None and kwargs['device'] == 'gpu') else np
+    return Tensor(xp.zeros(*shape,dtype=np.float32),requires_grad=requires_grad,**kwargs)
+def ones(*shape,requires_grad=False,**kwargs):
+    xp = cp  if (cp is not None and kwargs['device'] == 'gpu') else np
+    return Tensor(xp.ones(*shape,dtype=np.float32),requires_grad=requires_grad,**kwargs)
+def empty(*shape,requires_grad=False,**kwargs):
+    xp = cp  if (cp is not None and kwargs['device'] == 'gpu') else np
+    return Tensor(xp.empty(*shape,dtype=np.float32),requires_grad=requires_grad,**kwargs)
+def rand(*shape,requires_grad=False,**kwargs):
+    xp = cp  if (cp is not None and kwargs['device'] == 'gpu') else np
+    return Tensor(xp.random.rand(*shape,dtype=np.float32),requires_grad=requires_grad,**kwargs)
+def randn(shape,requires_grad=False,**kwargs):
+    return Tensor(xp.random.default_rng().standard_normal(size=shape, dtype=np.float32),requires_grad=requires_grad,**kwargs)
+def randint(low,high=None,size=None,requires_grad=False,**kwargs):
+    return Tensor(xp.random.randint(low,high=high,size=size),requires_grad=requires_grad,**kwargs)
+def arange(stop,start=0,requires_grad=False,**kwargs):
+    xp = cp  if (cp is not None and kwargs['device'] == 'gpu') else np
+    return Tensor(xp.arange(start=start,stop=stop,dtype=np.float32),requires_grad=requiress_grad,**kwargs)
+def zeros_like(t,requires_grad=False,**kwargs):return zeros(*t.shape,requires_grad=requires_grad,**kwargs)
+def ones_like(t,requires_grad=False,**kwargs):return ones(*t.shape,requires_grad=requires_grad,**kwargs)
+def empty_like(t,requires_grad=False,**kwargs):return empty(*t.shape,requires_grad=requires_grad,**kwargs)
+def randn_like(t,requires_grad=False,**kwargs):return randn(t.shape,requires_grad=requires_grad,**kwargs)
 
 
