e282e0a2231f50c090eb0b5664c03e26eac6dbff
zolda93 <aybayb9393@gmail.com>
2024-09-25 15:28:49 +0100

    fixing

--- elysium/tensor.py
+++ elysium/tensor.py
@@ -85,8 +85,9 @@ class Tensor:
         return Tensor(self.data.get(),requires_grad=self.requires_grad,device='cpu',dtype=self.data.dtype)
     def detach(self)->'Tensor':return Tensor(self.data,requires_grad=False,device=self.device,dtype=self.data.dtype)
     def zero_grad(self)->None:
-        if self.grad is not None:
-            self.grad.data.fill(0.)
+        self.grad = None
+        #if self.grad is not None:
+            #self.grad.datia.fill(0.)
     def __repr__(self)->str:
         grad_fn = ' ,grad_fn<' + self._ctx.func.__name__ + 'Backward' + str(self.device)+'>' if self._ctx is not None and self.requires_grad else ''
         dtype = f' ,dtype=elysium.{self.data.dtype}'
